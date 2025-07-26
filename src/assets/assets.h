89e6f24218c5194f02bc179dd0606dba2a069ed5
Marcos Vinícius Marcon <marconvcm@gmail.com>
2025-07-26 14:39:48 +0200

    Parcel assets

---
+++ src/assets/assets.h
@@ -0,0 +1,17 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <psxgpu.h>
+#include <psxgte.h>
+#include <inline_c.h>
+
+// region images
+
+extern u_long tim_game_title[];
+extern u_long tim_main_texture[];
+
+// endregion
+
+#endif // ASSETS_H
---
+++ tools/parcel/main.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+
+# Script to update CMakeLists.txt with TIM file entries
+# 
+# This script:
+# 1. Finds all .tim files in the assets directory
+# 2. Updates the CMakeLists.txt by adding psn00bsdk_target_incbin entries
+#    between the #region images and #endregion markers
+
+require 'fileutils'
+
+# Path configuration
+$script_dir = File.dirname(File.expand_path(__FILE__))
+$tools_root = File.expand_path('..', $script_dir)
+$project_root = File.expand_path('..', $tools_root)
+$src_dir = File.join($project_root, 'src')
+$assets_dir = File.join($src_dir, 'assets')
+$assets_file = File.join($assets_dir, 'assets.h')
+$cmake_file = File.join($src_dir, 'CMakeLists.txt')
+
+def parcel_tim_files
+   cmake_file = $cmake_file
+   # Find all .tim files in the assets directory
+   tim_files = Dir.glob(File.join($assets_dir, '*.tim')).map do |file|
+   # Get the relative path from the CMakeLists.txt file location
+   File.join('assets', File.basename(file))
+   end
+
+   puts "Found #{tim_files.length} TIM files:"
+   tim_files.each { |file| puts "  - #{file}" }
+
+   # Read the CMakeLists.txt file
+   cmake_content = File.read(cmake_file)
+   assets_content = File.read($assets_file)
+
+   # Create the new content to insert
+   cmake_includes = tim_files.map do |tim_file|
+      target_name = File.basename(tim_file, '.tim')
+      "psn00bsdk_target_incbin(hello_pong PRIVATE tim_#{target_name} #{tim_file})"
+   end
+
+   cmake_includes = cmake_includes.join("\n")
+
+   # Find the region markers and replace the content between them
+   if cmake_content.include?('#region images') && cmake_content.include?('#endregion')
+      updated_content = cmake_content.gsub(
+         /#region images\n.*?#endregion/m,
+         "#region images\n#{cmake_includes}\n#endregion"
+      )
+      File.write(cmake_file, updated_content)
+      puts "Successfully updated #{cmake_file} with #{tim_files.length} TIM file entries."
+   else
+      puts "Error: Could not find the region markers (#region images and #endregion) in #{cmake_file}"
+      exit 1
+   end
+
+   # Update assets.h with the TIM file entries
+   tim_entries = tim_files.map do |tim_file|
+      target_name = File.basename(tim_file, '.tim')
+      "extern u_long tim_#{target_name}[];\n"
+   end
+
+   # Write the updated content back to assets.h
+   if assets_content.include?('// region images') && assets_content.include?('// endregion')
+      updated_assets_content = assets_content.gsub(
+         /\/\/ region images\n.*?\/\/ endregion/m,
+         "// region images\n\n#{tim_entries.join}\n// endregion"
+      )
+      File.write($assets_file, updated_assets_content)
+      puts "Successfully updated #{$assets_file} with #{tim_files.length} TIM file entries."
+   else
+      puts "Error: Could not find the region markers (// region images and // endregion) in #{$assets_file}"
+      exit 1
+   end
+
+end
+
+parcel_tim_files if __FILE__ == $0
